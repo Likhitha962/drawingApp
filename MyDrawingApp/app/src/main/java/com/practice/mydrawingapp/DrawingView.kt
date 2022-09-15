ee737a19b286a1bfa04da9814e3a05525edde5b9
Likhitha <likkilikki047@gmail.com>
2022-09-15 23:46:43 +0530

    i've turned the drawn art to stay on the canvas until further instruction

--- MyDrawingApp/app/src/main/java/com/practice/mydrawingapp/DrawingView.kt
+++ MyDrawingApp/app/src/main/java/com/practice/mydrawingapp/DrawingView.kt
@@ -16,6 +16,8 @@ class DrawingView(context: Context,attrs:AttributeSet) : View(context, attrs) {
     private  var mBrushSize: Float = 0.toFloat()
     private  var color = Color.BLACK
     private var canvas : Canvas? = null
+    private  var mPaths = ArrayList<CustomPath>()
+
 
     init {
         setUpDrawing()
@@ -42,7 +44,14 @@ class DrawingView(context: Context,attrs:AttributeSet) : View(context, attrs) {
     //change canvas to canvas? if it fails
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+
     canvas.drawBitmap(mCanvasBitmap!!,0f,0f,mCanvasPaint)
+    for (path in mPaths){
+        mDrawPaint!!.strokeWidth = path.brushThickness
+        mDrawPaint!!.color =path.color
+        canvas.drawPath(path,mDrawPaint!!)
+    }
+
 if (!mDrawPath!!.isEmpty){
     mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
     mDrawPaint!!.color = mDrawPaint!!.color
@@ -66,6 +75,7 @@ if (!mDrawPath!!.isEmpty){
                         mDrawPath!!.lineTo(touchX!!, touchY!!)
             }
             MotionEvent.ACTION_UP -> {
+                mPaths.add(mDrawPath!!)
                 mDrawPath = CustomPath(color, mBrushSize)
             }
             else -> return false
